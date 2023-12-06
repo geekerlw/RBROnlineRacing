3161f9e1ac354d2fbc77ed34a56643c6e641ded1
Steven Lee <geekerlw@gmail.com>
2023-12-06 19:10:52 +0800

    api protocol initialize

---
+++ protocol/src/httpapi.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+pub static API_VERSION_STRING: &'static str = "v1.0";
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserLogin {
+    pub name: String,
+    pub passwd: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserAccess {
+    pub token: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RaceItem {
+    pub name: String,
+    pub stage: String,
+    pub owner: String,
+    pub state: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RaceList {
+    pub room: Vec<RaceItem>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RaceInfo {
+    pub name: String,
+    pub stage: String,
+    pub car: Option<String>,
+    pub damage: Option<u32>,
+    pub setup: Option<String>,
+    pub state: u32,
+    pub players: Vec<String>,
+}
