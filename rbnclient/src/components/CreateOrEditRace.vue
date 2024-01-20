252d188e5713a57ea25d91663dd2356d803e7849
hejia <jia_he@droidhang.com>
2024-01-20 14:36:41 +0800

    update-ui-创建比赛

---
+++ rbnclient/src/components/CreateOrEditRace.vue
@@ -0,0 +1,212 @@
+<template>
+  <!-- 利用element-plus的弹窗组件实现 -->
+  <el-dialog title="创建房间" v-model="dialogVisible" width="90%" top="5vh" class="dlog"  modal="false">
+    <el-form :model="form" size="small" :rules="rules" label-width="80px">
+      <div class="group-title">房间设定</div>
+      <el-form-item label="房间名" prop="name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="房间密码" prop="password">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+
+      <div class="group-title">比赛设定</div>
+      <el-form-item label="比赛赛道" prop="stage" class="multitem">
+        <el-select v-model="form.stage" placeholder="请选择地图" filterable>
+          <el-option
+            v-for="item in stageList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+        <el-button class="line-btn" @click="randomStage" type="success"
+          >随机一下</el-button
+        >
+      </el-form-item>
+      <!-- 车辆损坏 select -->
+      <el-form-item label="车辆损坏" prop="damage">
+        <el-select v-model="form.damage" placeholder="请选择车辆损坏">
+          <el-option
+            v-for="item in damageList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <!-- 车辆选择 select 支持搜索 -->
+      <el-form-item label="车辆选择" prop="car" class="multitem">
+        <el-select v-model="form.car" placeholder="请选择车辆" filterable>
+          <el-option
+            v-for="item in carList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+        <el-checkbox>
+          <span slot="label">限定车辆</span>
+        </el-checkbox>
+      </el-form-item>
+
+      <div class="group-title">条件设定</div>
+      <!-- 路面情况 -->
+      <el-form-item label="路面情况" prop="road">
+        <el-select v-model="form.road" placeholder="">
+          <el-option
+            v-for="item in roadList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <!-- 湿滑情况选择 -->
+      <el-form-item label="湿滑情况" prop="湿滑情况">
+        <el-select v-model="form.road" placeholder="">
+          <el-option
+            v-for="item in roadList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="比赛时段" prop="路面情况">
+        <el-select v-model="form.road" placeholder="">
+          <el-option
+            v-for="item in roadList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>      
+      <el-form-item label="比赛时段" prop="路面情况">
+        <el-select v-model="form.road" placeholder="">
+          <el-option
+            v-for="item in roadList"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary"> 确认 </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+<script setup>
+import { ref, reactive } from "vue";
+const dialogVisible = ref(false);
+const showCreateRace = () => {
+  dialogVisible.value = true;
+  console.log("showCreateRace");
+};
+const hideCreateRace = () => {
+  dialogVisible.value = false;
+};
+
+// 判断是否是编辑模式
+const editRaceId = ref("");
+
+const stageList = [
+  {
+    label: "hy",
+    value: "hy",
+  },
+  {
+    label: "com",
+    value: "com",
+  },
+];
+const damageList = [
+  {
+    label: "real",
+    value: "real",
+  },
+  {
+    label: "sim",
+    value: "sim",
+  },
+];
+const carList = [
+  {
+    label: "car1",
+    value: "car1",
+  },
+  {
+    label: "car2",
+    value: "car2",
+  },
+];
+const roadList = [
+  {
+    label: "road1",
+    value: "road1",
+  },
+  {
+    label: "road2",
+    value: "road2",
+  },
+];
+
+const rules = [];
+const form = reactive({
+  name: "11",
+  stage: "",
+  damage: "",
+  car: "",
+  road: '',
+});
+
+const randomStage = () => {
+  const index = Math.floor(Math.random() * stageList.length);
+  form.stage = stageList[index].value;
+};
+
+defineExpose({
+  showCreateRace,
+  hideCreateRace,
+});
+</script>
+<style lang="less" scoped>
+.group-title {
+  margin: 20px 0;
+  padding-left: 20px;
+  position: relative;
+  font-weight: bold;
+  text-align: left;
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 1px;
+    width: 10px;
+    background-color: #409eff; //更改为你想要的颜色
+  }
+}
+.multitem :deep(.el-form-item__content){
+  display: flex;
+  justify-content: space-between;
+}
+.multitem :deep(.el-select){
+  width: calc(100% - 90px);
+}
+:global(.el-dialog__body){
+  padding-top: 10px;
+}
+// .line-btn {
+//   margin-top: 10px;
+// }
+</style>
--- rbnclient/src/pages/Lobby.vue
+++ rbnclient/src/pages/Lobby.vue
@@ -1,81 +1,123 @@
 <template>
   <div class="lobby">
     <div class="title">
-        <div class="left-group">
-            <el-button class="rb" type="default" @click="goHome"><el-icon><ArrowLeft /></el-icon>返回</el-button>
-            <el-button class="cr" type="success" @click="back">创建房间</el-button>
-        </div>
-        {{$t('lobbyTitle')}}
+      <div class="left-group">
+        <el-button class="rb" type="default" @click="goHome"
+          ><el-icon><ArrowLeft /></el-icon>返回</el-button
+        >
+        <el-button class="cr" type="success" @click="showCreateRace"
+          >创建房间</el-button
+        >
+      </div>
+      {{ $t("lobbyTitle") }}
     </div>
     <div class="block20"></div>
-    <div class="roomlist">
-      <div class="item">
-        <div class="name line">房名: jakebless</div>
-        <div class="map line">地图: lyon-Geend</div>
-        <div class="roomowener line">房主: jakebless</div>
-        <div class="roomusers line">人数: 1/2</div>
-        <div class="status line">状态: 等待中</div>
+    <div class="racelist">
+      <div class="item" v-for="(race, index) in raceList" :key="race">
+        <div class="name line">房名: {{ race.name }}</div>
+        <div class="stage line">地图: {{ race.stage }}</div>
+        <div class="owner line">房主: {{ race.owner }}</div>
+        <div class="players line">人数: {{ race.players }}/8</div>
+        <div class="status line">状态: {{ stateText(race.state) }}</div>
         <div class="action">
-          <el-button type="primary" @click="back">加入</el-button>
-          <el-button type="danger" @click="back">退出比赛</el-button>
+          <el-button type="primary" v-if="race.state == '等待中'" @click="back"
+            >加入</el-button
+          >
+          <el-button type="danger" v-if="race.state == '进行中'" @click="back"
+            >退出比赛</el-button
+          >
         </div>
       </div>
     </div>
   </div>
+  <CreateOrEditRace ref="createRef"></CreateOrEditRace>
   <!-- <router-link :to="'/room/' + room">{{room}}</router-link> -->
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
+import CreateOrEditRace from "../components/CreateOrEditRace.vue";
+
 const router = useRouter();
 const goHome = () => {
   router.push("/home");
 };
-
 const back = () => {
   history.back();
 };
 
-const dataList = ref([]);
+const roomMockList = [
+  {
+    name: "jakebless",
+    stage: "lyon-Geend",
+    owner: "jakebless",
+    players: 5,
+    state: "等待中",
+  },
+];
+
+const raceList = ref([]);
+
+const stateText = (state) => {
+  switch (state) {
+    case "等待中":
+      return "等待中";
+    case "进行中":
+      return "进行中";
+    case "已结束":
+      return "已结束";
+    default:
+      return "未知";
+  }
+};
+
+const createRef = ref(null);
+const showCreateRace = () => {
+  createRef.value.showCreateRace();
+};
+
+const ssadsds = ref(null);
+
 onMounted(() => {
-  dataList.value = ["room1", "room2", "room3"];
+  raceList.value = roomMockList;
+  showCreateRace();
 });
 </script>
 
 <style lang="less" scoped>
-.lobby{
-    width: 100%;
-    cursor: default;
+.lobby {
+  width: 100%;
+  cursor: default;
 }
-.title{
-    font-size: 30px;
-    font-weight: bold;
-    text-align: center;
-    margin-bottom: 20px;
-    position: relative;
-    margin: 0 auto;
-    width: 90%;
-    .left-group{
-        left: 0;
-        position: absolute;
+.title {
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+  position: relative;
+  margin: 0 auto;
+  width: 90%;
+  .left-group {
+    left: 0;
+    position: absolute;
+  }
+}
+.racelist {
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: flex-start;
+  .item {
+    width: 200px;
+    border: 1px solid #ededed;
+    padding: 20px;
+    border-radius: 5px;
+    background: #f8f8ff;
+    box-shadow: 0 0 10px #ededed;
+    .line {
+      margin-bottom: 10px;
     }
+  }
 }
-.roomlist{
-    display: flex;
-    width: 90%;
-    margin: 0 auto;
-    justify-content: flex-start;
-    .item{
-        width: 200px;
-        border: 1px solid #ededed;
-        padding: 20px;
-        border-radius: 5px;
-        background: #f8f8ff;
-        box-shadow: 0 0 10px #ededed;
-        .line{
-            margin-bottom: 10px;
-        }
-    }
-}
-</style>>
+</style>
