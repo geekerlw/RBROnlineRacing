b230b597fa77df113d2a701e421b4be4b5cfe951
hejia <jia_he@droidhang.com>
2024-01-23 18:43:05 +0800

    addroomstate

---
+++ rbnclient/src/pages/RoomRaceState.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="racingState">
+    游戏加载中...
+    <div class="user">
+      <div class="item">race1: 加载中</div>
+      <div class="item">race2: 加载完成</div>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+</style>
