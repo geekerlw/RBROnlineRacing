254292ada0dc453aade13bd5039236487599f418
hejia <jia_he@droidhang.com>
2024-02-19 15:34:10 +0800

    update-api-url创建比赛

--- rbnclient/src/api/index.js
+++ rbnclient/src/api/index.js
@@ -12,14 +12,9 @@ export const login = (data) => {
 };
 
 export const createRace = (data) => {
-  return httpJson("/api/user/login", {
+  return httpJson("/api/race/create", {
     method: "POST",
     responseType: 2, // 1 to Json, 2 to Text, 3 to Binary
-    data: {
-      name: data.name,
-      description: data.description,
-      start_time: data.start_time,
-      end_time: data.end_time,
-    },
+    data: data
   });
 };
