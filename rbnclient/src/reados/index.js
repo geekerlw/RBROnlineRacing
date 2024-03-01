cf9de60cf7a0ab8580b5f043a6f32f9fa564de04
Steven Lee <geekerlw@gmail.com>
2024-03-01 18:03:09 +0800

    add some api define

--- rbnclient/src/reados/index.js
+++ rbnclient/src/reados/index.js
@@ -11,3 +11,56 @@ export async function get_user_name() {
   let value = await invoke("get_user_name");
   return value
 }
+
+
+/*
+ * Common functions, like config store and game config load etc.
+ */
+export async function load_store_config() {
+
+}
+
+export async function save_store_config() {
+
+}
+
+export async function load_game_user_config() {
+
+}
+
+export async function load_game_stage_options() {
+
+}
+
+export async function load_game_stage_wetness_options() {
+
+}
+
+export async function load_game_stage_weather_options() {
+
+}
+
+export async function load_game_stage_skytype_options() {
+
+}
+
+export async function load_game_car_options() {
+
+}
+
+export async function load_game_car_damage_options() {
+
+}
+
+export async function load_game_car_tyre_options() {
+
+}
+
+export async function load_game_car_setup_options() {
+
+}
+
+
+/*
+ * Game flow control functions.
+ */
