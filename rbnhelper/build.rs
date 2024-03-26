59c4b67883caf89482119c628dee84fbcea1437d
Steven Lee <geekerlw@gmail.com>
2024-03-26 18:10:32 +0800

    update dll windows resource pack support

--- rbnhelper/build.rs
+++ rbnhelper/build.rs
@@ -1,3 +1,4 @@
+extern crate winres;
 use std::path::Path;
 use std::{env, path::PathBuf};
 
@@ -6,4 +7,9 @@ fn main() {
 	let root = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
 	println!("cargo:rustc-link-lib=static={}", library_name);
 	println!("cargo:rustc-link-search=native={}", Path::new(&root).join("rbr").display());
+
+	if cfg!(target_os = "windows") {
+		let res = winres::WindowsResource::new();
+		res.compile().unwrap();
+	}
 }
