871a3683e38345b7f8de2ff03046debe30f4e71f
Steven Lee <geelerlw@gmail.com>
2024-03-26 21:45:37 +0800

    change way to set player timeout

--- rbnhelper/src/components/player.rs
+++ rbnhelper/src/components/player.rs
@@ -6,38 +6,34 @@ use rodio::{Decoder, OutputStream, source::Source};
 #[derive(Default)]
 pub struct OggPlayer {
     file: PathBuf,
+    timeout: u64,
 }
 
 impl OggPlayer {
     pub fn open(file: &str) -> Self {
+        let mut player = OggPlayer::default();
+        player.timeout = 3; // default play 3 seconds.
         if let Some(game_root) = std::env::current_exe().unwrap().parent() {
-            return Self { file: game_root.join("Plugins").join("RBNHelper").join("audio").join(file) };
+            player.file = game_root.join("Plugins").join("RBNHelper").join("audio").join(file);
         }
-        Self { file: PathBuf::new() }
+        player
+    }
+
+    pub fn set_timeout(mut self, secs: u64) -> Self {
+        self.timeout = secs;
+        self
     }
 
     pub fn play(&mut self) {
         if self.file.exists() && self.file.is_file() {
             let audio_file = self.file.clone();
+            let timeout = self.timeout.clone();
             std::thread::spawn(move || {
                 let (_stream, stream_handle) = OutputStream::try_default().unwrap();
                 let file = BufReader::new(File::open(audio_file).unwrap());
                 let source = Decoder::new(file).unwrap();
                 stream_handle.play_raw(source.convert_samples()).unwrap();
-                std::thread::sleep(std::time::Duration::from_secs(3));
-            });
-        }
-    }
-
-    pub fn play_with_time(&mut self, secs: u64) {
-        if self.file.exists() && self.file.is_file() {
-            let audio_file = self.file.clone();
-            std::thread::spawn(move || {
-                let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-                let file = BufReader::new(File::open(audio_file).unwrap());
-                let source = Decoder::new(file).unwrap();
-                stream_handle.play_raw(source.convert_samples()).unwrap();
-                std::thread::sleep(std::time::Duration::from_secs(secs));
+                std::thread::sleep(std::time::Duration::from_secs(timeout));
             });
         }
     }
