9fd2d205e348b574253b609902447822c24b2f18
Steven Lee <geelerlw@gmail.com>
2024-04-13 15:28:09 +0800

    fix hotlap chooise error

--- rbnserver/src/series/daily.rs
+++ rbnserver/src/series/daily.rs
@@ -243,7 +243,7 @@ impl Daily {
                 self.pit.notify_all_players_race_notice(format!("Please wait, {} players is still in racing, maybe finished in {} seconds.", self.room.players.len(), self.room.guess_race_remain()));
                 self.room.notify_all_players_race_state();
             } else {
-                self.pit.notify_all_players_race_notice(format!("Next Race will be start at [{}], remain time [{}]", self.start_time, self.start_time - Local::now()));
+                self.pit.notify_all_players_race_notice(format!("Next Race will start at {}, remain {} seconds.", self.start_time, (self.start_time - Local::now()).num_seconds()));
                 self.pit.notify_all_players_race_state();
             }
         }
