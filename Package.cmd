4f8d294cc180d75b6bb6652a1603a50d2fb805d9
Steven Lee <geelerlw@gmail.com>
2024-04-29 21:05:00 +0800

    fix windows package error

--- Package.cmd
+++ Package.cmd
@@ -43,7 +43,7 @@ echo Do you want to create the pkg RELEASE\%RELEASE_PKG%?
 echo Press CTRL-C to quit, other key to continue...
 pause
 
-cargo build --release --package rbnhelper
+cargo build --release --package rbnhelper --target i686-pc-windows-msvc
 
 rem Create release folders and copy the release versions of plugin files there
 mkdir "%RELEASE_FOLDER%\"
