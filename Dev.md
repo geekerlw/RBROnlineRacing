a5ef156429d8983381e36c848951ef79558277c2
Steven Lee <geekerlw@gmail.com>
2024-04-29 11:29:15 +0800

    add package.sh and development.md

--- Dev.md
+++
@@ -1,7 +0,0 @@
-## RUN RBN_server
-```
-cd rbnserver;
-export RUST_LOG=info;
-cargo run
-```
-web: http://127.0.0.1:23555/
--- Package.sh
+++ Package.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
 echo "Current Path: $ROOT_PATH"
 
-if [ "$1" == "release" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "Error missing argument, pack failed."
+	echo "Usage: ./package.sh [version] [debug|release]"
+	echo "example: ./package.sh 1.1.0 release"
+	exit 1
+fi
+
+if [ "$2" == "release" ]; then
         TARGET_DIR="$ROOT_PATH/target/release"
 else
         TARGET_DIR="$ROOT_PATH/target/debug"
@@ -11,16 +18,50 @@ fi
 
 cd "$ROOT_PATH/rbnserver"
 
-if [ "$1" == "release" ]; then
+if [ "$2" == "release" ]; then
         cargo build --release
 else
         cargo build
 fi
 
+# backup rbndata.db file
+if [ -f "$TARGET_DIR/rbndata.db" ]; then
+	cp -rvf "$TARGET_DIR/rbndata.db" "$TARGET_DIR/rbndata.db.bak"
+fi
+
+# copy assets to target dir
 cp -rvf "$ROOT_PATH/rbnserver/migrations" "$TARGET_DIR"
 cp -rvf "$ROOT_PATH/rbnserver/rsfdata" "$TARGET_DIR"
 cp -rvf "$ROOT_PATH/rbnserver/templates" "$TARGET_DIR"
 
-echo "Package over."
+
+if [ "$2" == "release" ]; then
+	RELEASE_DIR="$ROOT_PATH/Release"
+	RELEASE_TEMP="$RELEASE_DIR/RBNServer"
+	RELEASE_FILE="RBNServer_$1.tar.gz"
+
+	if [ -d "$RELEASE_DIR/RBNServer" ]; then
+		if [ -f "$RELEASE_TEMP/rbndata.db" ]; then
+			cp -a "$RELEASE_TEMP/rbndata.db" "$RELEASE_DIR/rbndata.db.bak"
+		fi
+		rm -rvf "$RELEASE_TEMP"
+	fi
+
+	mkdir "$RELEASE_DIR/RBNServer"
+	cp -rvf "$ROOT_PATH/rbnserver/migrations" "$RELEASE_TEMP"
+	cp -rvf "$ROOT_PATH/rbnserver/rsfdata" "$RELEASE_TEMP"
+	cp -rvf "$ROOT_PATH/rbnserver/templates" "$RELEASE_TEMP"
+	cp -rvf "$TARGET_DIR/rbnserver" "$RELEASE_TEMP"
+
+	cd "$RELEASE_DIR"
+	tar -czf $RELEASE_FILE RBNServer
+	cd -
+
+	echo "release over, release file is: $RELEASE_FILE"
+fi
+
+# copy assets to release dir
+
+echo "pack over, out dir is: $TARGET_DIR"
 
 exit
